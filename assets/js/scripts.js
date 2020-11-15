00aec8f8ad898a1eb66c19e4a40aea6a49e8ee42
Rishav Galhotra <rishavgalhotra95@gmail.com>
2020-11-15 23:35:01 +0530

    faq

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -396,6 +396,21 @@ LANDING PAGE JS
 	// 		return false;
 	// 	});
 	// });
+
+
+
+		$('.pro .lpnspeed-value > span').each(function(){
+			$(this).prop('Counter',0).animate({
+				Counter: $(this).text()
+			},{
+				duration: 3500,
+				easing: 'swing',
+				step: function (now){
+					$(this).text(Math.ceil(now));
+				}
+			});
+		});
+	
 			
 })(jQuery);
 
